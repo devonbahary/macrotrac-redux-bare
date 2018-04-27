17da3d708165568563cd7ad0eeb5852e8b999ac2
devonbahary <devonbahary@gmail.com>
2018-04-27 13:39:38 -0700

    Update FoodsNotification.js component to display result of Foods actions

--- src/components/foods/FoodsNotification.js
+++ src/components/foods/FoodsNotification.js
@@ -1,52 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const FoodsNotification = (props) => {
-    if (props.foods.isFetching) {
-        return (
-            <p className="confirmed">Loading your food..</p>
-        );
-    } else if (props.foods.failedLoad) {
-        return (
-            <p className="error">Failed to load your food.</p>
-        );
-    } else if (props.foods.isAdding) {
-        return (
-            <p className="confirmed">Adding new food item..</p>
-        );
-    } else if (props.foods.failedAdd) {
-        return (
-            <p className="error">Failed to add new food item.</p>
-        );
-    } else if (props.foods.isEditing) {
-        return (
-            <p className="confirmed">Editing food item..</p>
-        );
-    } else if (props.foods.failedEdit) {
-        return (
-            <p className="error">Failed to edit food item.</p>
-        );
-    } else if (props.foods.isRemoving) {
-        return (
-            <p className="confirmed">Removing food item..</p>
-        );
-    } else if (props.foods.failedRemove) {
-        return (
-            <p className="error">Failed to remove food item.</p>
-        );
-    } else if (props.foods.items.length > 0) {
-        return (
-            <p>Found {props.foods.items.length} foods:</p>
-        );
-    } else {
-        return (
-            <p>No food items found.</p>
-        );
-    }
-};
+const FoodsNotification = (props) => (
+    <p className="confirmed">{props.notification}</p>
+);
 
 const mapStateToProps = (state) => ({
-  foods: state.foods
+  notification: state.foods.notification
 });
 
 export default connect(mapStateToProps)(FoodsNotification);
--- src/reducers/foods.js
+++ src/reducers/foods.js
@@ -2,7 +2,8 @@ import uuid from 'uuid';
 
 
 const initialFoodsState = {
-  items: []
+  items: [],
+  notification: ''
 };
 
 export default (prevState = initialFoodsState, action) => {
@@ -11,25 +12,29 @@ export default (prevState = initialFoodsState, action) => {
         case 'FETCH_FOODS':
             items = localStorage.getItem('foods');
             return {
-              items: items ? JSON.parse(items) : prevState.items
+              items: items ? JSON.parse(items) : prevState.items,
+              notification: `Found ${JSON.parse(items).length} foods.`
             };
         case 'ADD_FOOD':
             items = [ ...prevState.items, { ...action.food, id: uuid() } ];
             localStorage.setItem('foods', JSON.stringify(items));
             return {
-              items
+              items,
+              notification: 'Added new food item.'
             };
         case 'EDIT_FOOD':
             items = prevState.items.map(food => food.id === action.id ? { ...food, ...action.updates } : food);
             localStorage.setItem('foods', JSON.stringify(items));
             return {
-              items
+              items,
+              notification: 'Updated food item.'
             };
         case 'REMOVE_FOOD':
             items = prevState.items.filter(food => food.id !== action.id);
             localStorage.setItem('foods', JSON.stringify(items));
             return {
-              items
+              items,
+              notification: 'Removed food item.'
             };
         default:
             return prevState;
